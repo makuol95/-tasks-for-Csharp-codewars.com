6e03f33d471deafbeb358b84025c653af298f023
makuol95 <72643122+makuol95@users.noreply.github.com>
2021-06-03 13:08:14 +0300

    add new task
    добавление новых задач

--- tetsprogramwar/Program.cs
+++ tetsprogramwar/Program.cs
@@ -8,8 +8,26 @@ namespace tetsprogramwar
     {
         static void Main(string[] args)
         {
-           
-           
+            int[][] array =
+            {
+                new int [0]
+                /*new []{1, 2, 3,4,5},
+                new []{6,7, 8,9,10},
+                new []{11, 12, 13,14,15},                
+                new []{16,17, 18,19,20},
+                new []{21, 22, 23,24,25}*/
+            };
+            foreach (int [] a in array)
+            {
+                foreach (int b in a)
+                    Console.Write("{0,4}",b);
+                Console.WriteLine();
+            }
+            var result = SnailSolution.Snail(array);
+            foreach(int a in result)
+            {
+                Console.Write($"{a} ");
+            }
             
         }
     }
---
+++ tetsprogramwar/SnailSolution.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tetsprogramwar
+{
+    class SnailSolution
+    {
+        public static int[] Snail(int[][] array)
+        {
+            if(array.Length==0||array.Any(r=>r.Length==0))
+            {
+               return new int [0];
+                
+            }
+            
+            int[] resuktarray = new int[array.Length * array.Length];
+            int a = 0, b = 1, j = 0,i=0 ;
+            int flag = 0;
+            int c = (array.Length / 2);
+            for(int k=0; k<resuktarray.Length&&c>a;k++)
+            {
+                switch(flag)
+                {
+                    case 0:
+                        {
+                            
+                            if (a<=j&&j< array.Length-a-b)
+                            {
+                                resuktarray[k] = array[i][j];
+                                j++;
+                            }
+                            else
+                            {
+                                k--;
+                                flag = 1;
+                            }    
+                            break;
+                        }
+                    case 1:
+                        {
+                            if (i >=a  && i < array.Length - a -b)
+                            {
+                                resuktarray[k] = array[i][j];
+                                i++;
+                            }
+                            else
+                            {
+                                k--;
+                                flag = 2;
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            if (a+b <= j && j < array.Length - a )
+                            {
+                                resuktarray[k] = array[i][j];
+                                j--;
+                            }
+                            else
+                            {
+                                k--;
+                                flag = 3;
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            if (i >= a+b && i < array.Length - a )
+                            {
+                                resuktarray[k] = array[i][j];
+                                i--;
+                            }
+                            else
+                            {
+                                k--;
+                                flag = 0;
+                                a++;
+                                j = a;
+                                i = a;                                
+                            }
+                            break;
+                        }
+                }               
+            }
+            if (array.Length % 2!=0)
+                resuktarray[resuktarray.Length-1] = array[c][c];
+            return resuktarray;
+        }
+    }
+}
